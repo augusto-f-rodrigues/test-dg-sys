184263bd53ada95199cb4f7f4a5576fd567621ad
Augusto Rodrigues <85464318+augusto-f-rodrigues@users.noreply.github.com>
2023-09-28 14:33:13 -0300

    fix: removing console.log

--- index.js
+++ index.js
@@ -7,19 +7,14 @@ import { mainTest4 } from "./test4.js";
 import { mainTest5 } from "./test5.js";
 
 const main = async () => {
-  // console.log("### TESTE 1 ###");
   const resultTeste1 = await csvToJson("./archiveTest1.csv");
-
-  // console.log("### TESTE 2 ###");
+  
   const responseTeste2 = await filterByCity("Indaial");
 
-  // console.log("### TESTE 3 ###");
   const responseTest3 = await filterByColAndOrder("idade", "ASC");
 
-  // console.log("### TESTE 4 ###");
   mainTest4();
 
-  // console.log("### TESTE 5 ###");
   mainTest5();
 };
 
